f729a2fc36c4d12094cc439ff2899db462bcb94d
tolentinoel <rckrleyn@gmail.com>
2020-11-28 13:23:14 -0800

    commit tree 5e68b482ce46b58a4a2d9fdc3492305b200994d1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(d =>
+        d.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(d =>
+        d.charAt(0) === string.charAt(0) && d.charAt(1) === string.charAt(1)
+        )
+}
+
+function matchName(drivers, string){
+    return drivers.filter(d =>
+        d.name === string)
+}
